02802ef7f19dd1423a5b3055302e55ac980773cf
Joy Das <joykrishan10@gmail.com>
2024-02-29 19:15:31 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An End-to-End Neural Approach for Learning Patch Correctness in Automated Program Repair',
+    author='Joy Krishan Das, SR lab',
+    license='MIT',
+)
